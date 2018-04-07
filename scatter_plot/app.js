bdd54cf8eb3bbc0102fb7a986dc9f3471fc65d24
Christopher <jcp.christopher@gmail.com>
2018-04-07 17:52:20 +0530

    reenforcing scatter plots

--- scatter_plot/app.js
+++ scatter_plot/app.js
@@ -31,22 +31,19 @@ var y_scale = d3.scaleLinear()
         })])
         .range([chart_height - padding, padding]);
 
-var r_scale = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) {
-          return d[1];
-        })])
-        .range([5, 30]);
-
-var a_scale = d3.scaleSqrt()
-        .domain([0, d3.max(data, function(d) {
-          return d[1];
-        })])
-        .range([0, 25]);
-
 var x_axis = d3.axisBottom(x_scale);
 
 var y_axis = d3.axisLeft(y_scale)
             .ticks(5);
+
+// clip path
+svg.append('clipPath')
+   .attr('id', 'plot-area-clip-path')
+   .append('rect')
+   .attr('x', padding)
+   .attr('y', padding)
+   .attr('width', chart_width - padding * 3)
+   .attr('height', chart_height - padding * 2);
 
 svg.append('g')
    .attr('class', 'x-axis')
@@ -65,7 +62,10 @@ svg.append('g')
   .call(y_axis);
 
 //create circles
-svg.selectAll('circle')
+svg.append('g')
+   .attr('id', 'plot-area')
+   .attr('clip-path', 'url(#plot-area-clip-path)')
+   .selectAll('circle')
    .data(data)
    .enter()
    .append('circle')
@@ -75,23 +75,62 @@ svg.selectAll('circle')
    .attr('cy', function(d) {
      return y_scale(d[1]);
    })
-   .attr('r', function(d) {
-     return a_scale(d[1]);
-   })
+   .attr('r', 15)
+   // .attr('r', function(d) {
+   //   return a_scale(d[1]);
+   // })
    .attr('fill', '#D1AB0E');
 
-//create labels
-svg.append('g')
-   .selectAll('text')
-   .data(data)
-   .enter()
-   .append('text')
-   .text(function(d) {
-     return d.join(',');
-   })
-   .attr('x', function(d) {
-     return x_scale(d[0]);
-   })
-   .attr('y', function(d) {
-     return y_scale(d[1]);
-   });
+//events
+
+d3.select('button')
+  .on('click', function() {
+    // create random data
+    data = [];
+
+    var max_num = Math.random() * 1000;
+
+    for(var x = 0; x < 8; x++) {
+      var new_x = Math.floor(Math.random() * max_num);
+      var new_y = Math.floor(Math.random() * max_num);
+
+      data.push([new_x, new_y]);
+    }
+
+    // update scales
+    x_scale.domain([0, d3.max(data, function(d) {
+      return d[0];
+    })]);
+
+    y_scale.domain([0, d3.max(data, function(d) {
+      return d[1];
+    })]);
+
+    var colors = ['#F26D6D', '#1E6190', '#7559D9', '#D1AB03'];
+
+    var color_index = Math.floor(Math.random() * colors.length);
+
+    svg.selectAll('circle')
+      .data(data)
+      .transition()
+      .duration(1000)
+      .attr('cx', function(d) {
+        return x_scale(d[0]);
+      })
+      .attr('cy', function(d) {
+        return y_scale(d[1]);
+      })
+      .transition()
+      .attr('fill', colors[color_index]);
+
+    //update axis
+    svg.selectAll('.x-axis')
+      .transition()
+      .duration(1000)
+      .call(x_axis);
+
+    svg.selectAll('.y-axis')
+      .transition()
+      .duration(1000)
+      .call(y_axis);
+  });
